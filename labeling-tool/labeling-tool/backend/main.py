b16523786e8ee5074dcbd86a92a9c970a9cb5bd0
taraneh2abd <taraneh.abdellahi2@gmail.com>
2025-09-22 11:23:45 +0330

    correct bordering

--- labeling-tool/labeling-tool/backend/main.py
+++ labeling-tool/labeling-tool/backend/main.py
@@ -104,7 +104,9 @@ BATCHES = [
     },
     {
         "queryImage": "/images/img_73.png",
-        "images": ["/images/img_73.png", "/images/img_74.png", "/images/img_75.png", "/images/img_76.png","/images/img_73.png", "/images/img_84.png", "/images/img_85.png", "/images/img_80.png"]
+        "images": ["/images/img_940.png", "/images/img_941.png", "/images/img_942.png", "/images/img_953.png","/images/img_954.png", "/images/img_965.png", "/images/img_966.png", "/images/img_977.png",
+                   "/images/img_948.png", "/images/img_949.png", "/images/img_940.png", "/images/img_951.png","/images/img_952.png", "/images/img_963.png", "/images/img_964.png", "/images/img_975.png"
+                   ]
     },
     {
         "queryImage": "/images/img_82.png",
