1df475b9de9805a15ec1945e90b5a7f44719824b
taraneh2abd <taraneh.abdellahi2@gmail.com>
2025-09-22 11:56:08 +0330

    fix(front)better ui

--- labeling-tool/labeling-tool/backend/main.py
+++ labeling-tool/labeling-tool/backend/main.py
@@ -105,6 +105,8 @@ BATCHES = [
     {
         "queryImage": "/images/img_73.png",
         "images": ["/images/img_940.png", "/images/img_941.png", "/images/img_942.png", "/images/img_953.png","/images/img_954.png", "/images/img_965.png", "/images/img_966.png", "/images/img_977.png",
+                   "/images/img_948.png", "/images/img_949.png", "/images/img_940.png", "/images/img_951.png","/images/img_952.png", "/images/img_963.png", "/images/img_964.png", "/images/img_975.png",
+                   "/images/img_940.png", "/images/img_941.png", "/images/img_942.png", "/images/img_953.png","/images/img_954.png", "/images/img_965.png", "/images/img_966.png", "/images/img_977.png",
                    "/images/img_948.png", "/images/img_949.png", "/images/img_940.png", "/images/img_951.png","/images/img_952.png", "/images/img_963.png", "/images/img_964.png", "/images/img_975.png"
                    ]
     },
